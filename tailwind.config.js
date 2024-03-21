f9b948fa75963f89534c1abaa60a87c405d3b404
cslemes <cslemess@gmail.com>
2024-03-21 12:40:50 -0300

    add custom tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -72,7 +72,47 @@ module.exports = {
     extend: {
       colors: {
         dark: '#111'
+      },
+
+      animation: {
+        'gradient-x':'gradient-x 15s ease infinite',
+        'gradient-y':'gradient-y 15s ease infinite',
+        'gradient-xy':'gradient-xy 15s ease infinite',
+      },
+      keyframes: {
+          'gradient-y': {
+              '0%, 100%': {
+                  'background-size':'400% 400%',
+                  'background-position': 'center top'
+              },
+              '50%': {
+                  'background-size':'200% 200%',
+                  'background-position': 'center center'
+              }
+          },
+          'gradient-x': {
+              '0%, 100%': {
+                  'background-size':'200% 200%',
+                  'background-position': 'left center'
+              },
+              '50%': {
+                  'background-size':'200% 200%',
+                  'background-position': 'right center'
+              }
+          },
+          'gradient-xy': {
+              '0%, 100%': {
+                  'background-size':'400% 400%',
+                  'background-position': 'left center'
+              },
+              '50%': {
+                  'background-size':'200% 200%',
+                  'background-position': 'right center'
+              }
+          }
       }
+
+
     }
   },
   darkMode: ['class', 'html[class~="dark"]']
